19968c0c37f5a95e89bf29a50f6749516d086033
Andrew Comeau <acomeau@drewslair.com>
2025-09-02 21:50:21 -0400

    Uploaded WordCount.py

---
+++ WordCount.py
@@ -0,0 +1,18 @@
+import sys
+import io
+
+wordDict = {}
+
+if len(sys.argv) > 1:
+    with io.open(sys.argv[1], 'r') as f:
+        text = f.read().lower()
+
+    wordSet = set(text.translate(str.maketrans("", "", ".,\"")).split())
+    for word in wordSet:
+        wordDict[word] = text.count(word)
+
+    wordsByCount = dict(sorted(wordDict.items(), key=lambda item: item[1], reverse=True))
+
+    for word, count in wordsByCount.items():
+        print(f"{word}: {count}")
+
