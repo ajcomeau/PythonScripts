4925a9b093ad11e09ad05c103b9d0a86498fc9bf
Andrew Comeau <acomeau@drewslair.com>
2025-09-02 21:55:22 -0400

    Update WordCount.py

--- WordCount.py
+++ WordCount.py
@@ -8,14 +8,19 @@ wordDict = {}
 if len(sys.argv) > 1:
     with io.open(sys.argv[1], 'r') as f:
         text = f.read().lower()
-
+    # Remove punctuation
     wordSet = set(text.translate(str.maketrans("", "", ".,\"")).split())
+    
+    # Create dictionary of words
     for word in wordSet:
         wordDict[word] = text.count(word)
 
+    # Sort
     wordsByCount = dict(sorted(wordDict.items(), key=lambda item: item[1], reverse=True))
 
+    # Print
     for word, count in wordsByCount.items():
         print(f"{word}: {count}")
+
 
 
